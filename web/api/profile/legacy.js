2d858fcb9a4dcb2ae16dc2b7db7448b71540f548
Vitor Ivens <vitorivens@gmail.com>
2026-08-13 19:20:13 -0300

    P04: adicionar API de legado

---
+++ web/api/profile/legacy.js
@@ -0,0 +1,5 @@
+'use strict';
+const{withErrors,ok,requireMethod}=require('../../lib/http');
+const{requireUser}=require('../../lib/auth');
+const cardProgression=require('../../lib/cardProgression');
+module.exports=withErrors(async(req,res)=>{if(!requireMethod(req,res,'GET'))return;const user=await requireUser(req,res);if(!user)return;const legacy=await cardProgression.getLegacyProfile(user.id);ok(res,{progressionEnabled:process.env.CARD_PROGRESSION_V2_ENABLED!=='false',legacy,thresholds:{material:cardProgression.MATERIAL_THRESHOLDS,border:cardProgression.BORDER_THRESHOLDS,legacy:cardProgression.LEGACY_THRESHOLDS}});});
