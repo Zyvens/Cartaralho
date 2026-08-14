27966cd7e43de1cf57cd9b663baa4c1b3d759619
Vitor Ivens <vitorivens@gmail.com>
2026-08-13 23:28:38 -0300

    P07: corrigir fixture de estado pré-start

--- web/tests/roomConfigP7.test.js
+++ web/tests/roomConfigP7.test.js
@@ -3,7 +3,7 @@ const test=require('node:test');
 const assert=require('node:assert/strict');
 const config=require('../lib/roomConfigP7');
 
-function room(overrides={}){return{creatorId:'1',state:'waiting_players',maxPlayers:6,pointsToWin:10,handSize:5,useStandardDeck:true,cardCreationEnabled:true,playerCardsEnabled:true,afkEnabled:true,buffsEnabled:false,rewardConfigSnapshot:null,players:new Map([['1',{active:true}],['2',{active:true}],['3',{active:true}]]),...overrides};}
+function room(overrides={}){return{creatorId:'1',state:'aguardando_jogadores',maxPlayers:6,pointsToWin:10,handSize:5,useStandardDeck:true,cardCreationEnabled:true,playerCardsEnabled:true,afkEnabled:true,buffsEnabled:false,rewardConfigSnapshot:null,players:new Map([['1',{active:true}],['2',{active:true}],['3',{active:true}]]),...overrides};}
 
 test('normalização permite AFK desligado e mantém buffs apenas como flag',()=>{
  const c=config.initial({maxPlayers:10,pointsToWin:20,handSize:15,afkEnabled:false,cardCreationEnabled:false,playerCardsEnabled:false,buffsEnabled:true});
