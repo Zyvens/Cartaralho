5702341976831c0afd07ad495a04b29a372e824c
Vitor Ivens <vitorivens@gmail.com>
2026-08-14 19:55:42 -0300

    P18: expõe Central de Notificações

---
+++ web/api/notifications.js
@@ -0,0 +1,9 @@
+'use strict';
+const{withErrors,ok,requireMethod}=require('../lib/http');
+const{requireUser}=require('../lib/auth');
+const notifications=require('../lib/appNotifications');
+module.exports=withErrors(async(req,res)=>{
+ if(!requireMethod(req,res,'GET'))return;
+ const user=await requireUser(req,res);if(!user)return;
+ ok(res,await notifications.center(user.id));
+});
