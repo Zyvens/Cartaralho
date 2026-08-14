8a04a40432cebdfff289d0ce5b1184ecb051f04e
Vitor Ivens <vitorivens@gmail.com>
2026-08-13 23:18:47 -0300

    P07: atualizar configuração completa com trava pós-start

--- web/api/rooms/config.js
+++ web/api/rooms/config.js
@@ -1,17 +1,15 @@
 const { withErrors, ok, fail, requireMethod, getBody } = require('../../lib/http');
-const gameManager = require('../../lib/gameManager');
 const roomStore = require('../../lib/roomStore');
+const roomConfig = require('../../lib/roomConfigP7');
 
 module.exports = withErrors(async (req, res) => {
   if (!requireMethod(req, res, 'POST')) return;
   const { playerId, code, partialConfig } = getBody(req);
   if (!playerId || !code) return fail(res, 400, 'playerId e code são obrigatórios.');
-
   const room = await roomStore.loadRoom(code);
   if (!room) return fail(res, 404, 'Sala não encontrada.');
-
-  gameManager.updateRoomConfig(room, playerId, partialConfig || {});
+  try { roomConfig.apply(room, playerId, partialConfig || {}); }
+  catch (e) { return fail(res, 409, e.message); }
   await roomStore.saveRoom(room);
-
-  ok(res);
+  ok(res,{config:roomConfig.publicConfig(room)});
 });
