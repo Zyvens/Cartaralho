faf28ffcbc9bfb6c3f24633690087f2592a93c27
Vitor Ivens <vitorivens@gmail.com>
2026-08-14 01:49:10 -0300

    P08: expor feature flag pública de Buffs

--- web/api/config.js
+++ web/api/config.js
@@ -5,5 +5,5 @@ module.exports = withErrors(async (req, res) => {
   if (!requireMethod(req, res, 'GET')) return;
   const { PUSHER_KEY, PUSHER_CLUSTER } = process.env;
   if (!PUSHER_KEY || !PUSHER_CLUSTER) return fail(res, 500, 'PUSHER_KEY / PUSHER_CLUSTER não configuradas.');
-  ok(res, { pusherKey: PUSHER_KEY, pusherCluster: PUSHER_CLUSTER });
+  ok(res, { pusherKey: PUSHER_KEY, pusherCluster: PUSHER_CLUSTER, buffsFeatureEnabled: process.env.BUFFS_FEATURE_ENABLED !== 'false' });
 });
