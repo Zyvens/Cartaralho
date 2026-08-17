4000d375fa87f44a84aa7c2201a1c089505fbb1a
Vitor Ivens <vitorivens@gmail.com>
2026-08-17 07:24:29 -0300

    P45: expor versão 1.4.45

--- web/api/version.js
+++ web/api/version.js
@@ -1,4 +1,4 @@
 'use strict';
 const{withErrors,ok,requireMethod}=require('../lib/http');
-const{APP_VERSION}=require('../lib/releaseP44');
+const{APP_VERSION}=require('../lib/releaseP45');
 module.exports=withErrors(async(req,res)=>{if(!requireMethod(req,res,'GET'))return;ok(res,{currentVersion:APP_VERSION});});
