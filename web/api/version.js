ee8abbfd55bd3aecc45a1ccfd02198b86b6c46be
Vitor Ivens <vitorivens@gmail.com>
2026-08-18 00:21:30 -0300

    P58: expose current version

--- web/api/version.js
+++ web/api/version.js
@@ -1,4 +1,4 @@
 'use strict';
 const{withErrors,ok,requireMethod}=require('../lib/http');
-const{APP_VERSION}=require('../lib/releaseP57');
+const{APP_VERSION}=require('../lib/releaseP58');
 module.exports=withErrors(async(req,res)=>{if(!requireMethod(req,res,'GET'))return;ok(res,{currentVersion:APP_VERSION});});
