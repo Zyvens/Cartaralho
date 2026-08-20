7fa85eb6b4a6ba14c831f9c762e42ce1e41342ef
Vitor Ivens <vitorivens@gmail.com>
2026-08-20 00:24:48 -0300

    chore(release): publish P70 notification

--- web/api/notifications.js
+++ web/api/notifications.js
@@ -2,7 +2,8 @@
 const{withErrors,ok,requireMethod}=require('../lib/http');
 const{requireUser}=require('../lib/auth');
 const notifications=require('../lib/appNotifications');
-const{APP_VERSION,RELEASE}=require('../lib/releaseP69');
+const{APP_VERSION,RELEASE}=require('../lib/releaseP70');
+const{RELEASE:P69_RELEASE}=require('../lib/releaseP69');
 const{RELEASE:P68_RELEASE}=require('../lib/releaseP68');
 const{RELEASE:P67_RELEASE}=require('../lib/releaseP67');
 const{RELEASE:P66_RELEASE}=require('../lib/releaseP66');
@@ -39,6 +40,6 @@ module.exports=withErrors(async(req,res)=>{
  if(!requireMethod(req,res,'GET'))return;
  const user=await requireUser(req,res);if(!user)return;
  const data=await notifications.center(user.id);data.currentVersion=APP_VERSION;
- const releases=[RELEASE,P68_RELEASE,P67_RELEASE,P66_RELEASE,P65_RELEASE,P64_RELEASE,P63_RELEASE,P62_RELEASE,P61_RELEASE,P60_RELEASE,P59_RELEASE,P58_RELEASE,P57_RELEASE,P56_RELEASE,P55_RELEASE,P54_RELEASE,P53_RELEASE,P52_RELEASE,P51_RELEASE,P50_RELEASE,P49_RELEASE,P48_RELEASE,P47_RELEASE,P46_RELEASE,P45_RELEASE,P44_RELEASE,P43_RELEASE,P42_RELEASE,P41_RELEASE,P40_RELEASE,P39_RELEASE,P38_RELEASE,P37_RELEASE];
+ const releases=[RELEASE,P69_RELEASE,P68_RELEASE,P67_RELEASE,P66_RELEASE,P65_RELEASE,P64_RELEASE,P63_RELEASE,P62_RELEASE,P61_RELEASE,P60_RELEASE,P59_RELEASE,P58_RELEASE,P57_RELEASE,P56_RELEASE,P55_RELEASE,P54_RELEASE,P53_RELEASE,P52_RELEASE,P51_RELEASE,P50_RELEASE,P49_RELEASE,P48_RELEASE,P47_RELEASE,P46_RELEASE,P45_RELEASE,P44_RELEASE,P43_RELEASE,P42_RELEASE,P41_RELEASE,P40_RELEASE,P39_RELEASE,P38_RELEASE,P37_RELEASE];
  const blocked=new Set(releases.map(x=>x.id));data.updates=[...releases,...(data.updates||[]).filter(x=>!blocked.has(x.id))];ok(res,data);
 });
