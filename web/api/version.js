67d49f517e4915cd93c1e03932d662bd0e62b449
Vitor Ivens <vitorivens@gmail.com>
2026-08-21 12:43:33 -0300

    P75: expose current wallet startup fix

--- web/api/version.js
+++ web/api/version.js
@@ -1,4 +1,4 @@
 'use strict';
 const{withErrors,ok,requireMethod}=require('../lib/http');
-const{APP_VERSION}=require('../lib/releaseP74');
+const{APP_VERSION}=require('../lib/releaseP75');
 module.exports=withErrors(async(req,res)=>{if(!requireMethod(req,res,'GET'))return;ok(res,{currentVersion:APP_VERSION});});
