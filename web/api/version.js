889cda6052aca277861e96c30bb67e42764ebda4
Vitor Ivens <vitorivens@gmail.com>
2026-08-24 21:33:02 -0300

    release: expose v1.5.1 as current version

--- web/api/version.js
+++ web/api/version.js
@@ -1,8 +1,9 @@
 'use strict';
 const{withErrors,ok,requireMethod}=require('../lib/http');
-const{APP_VERSION}=require('../lib/releaseV15');
+const{APP_VERSION}=require('../lib/releaseV151');
+const{APP_VERSION:V15_VERSION}=require('../lib/releaseV15');
 const{APP_VERSION:P77_VERSION}=require('../lib/releaseP77');
 const{APP_VERSION:P76_VERSION}=require('../lib/releaseP76');
 const{APP_VERSION:P75_VERSION}=require('../lib/releaseP75');
-const RELEASE_LINEAGE=[P75_VERSION,P76_VERSION,P77_VERSION,APP_VERSION];
+const RELEASE_LINEAGE=[P75_VERSION,P76_VERSION,P77_VERSION,V15_VERSION,APP_VERSION];
 module.exports=withErrors(async(req,res)=>{if(!requireMethod(req,res,'GET'))return;ok(res,{currentVersion:APP_VERSION,releaseLineage:RELEASE_LINEAGE});});
